86bcc7d2f8ab0cd574fa81d37d8af311f6032c56
Shubham Deshmukh <Shubham Deshmukh@HNT-LT-287>
2026-08-26 18:40:54 +0530

    Add EF Core support and configure DI for services
    Added EF Core packages and SQL Server provider to .csproj. Introduced AppDBContext with DbSet<Character>. Registered AppDBContext and VideoGameCharacterServiceImpl in DI. Updated appsettings.json with DefaultConnection string.

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterApi.Models;
+
+namespace VideoGameCharacterApi.Data
+{
+    public class AppDBContext(DbContextOptions<AppDBContext> options) :DbContext(options)
+    {
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using VideoGameCharacterApi.Data;
 using VideoGameCharacterApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<AppDBContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IVideoGameCharacterService, VideoGameCharacterServiceImpl>();
 
 var app = builder.Build();
